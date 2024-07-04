80ed0398e4990034f40d45c799d1bd3898b3c57e
OmarSoub <omars.soub@gmail.com>
2024-07-04 18:26:08 +0300

    run template file

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "EndToEndMachineLearningProjectAutoMobilePrice"
+AUTHOR_USER_NAME = "omars1234"
+SRC_REPO = "AutoMobilePriceRegression"
+AUTHOR_EMAIL = "omars.soub@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python project for price feature ML model",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
